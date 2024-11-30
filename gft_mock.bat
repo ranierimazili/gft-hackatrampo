c83d82ebaa23543dcf1984a63c82e6783bb40e0f
Ranieri Mazili <ranieri85@gmail.com>
2024-11-30 09:44:10 -0300

    Modificado o gft_mock.bat para durar 15 segundos e usar a pasta temp

--- gft_mock.bat
+++ gft_mock.bat
@@ -1,4 +1,11 @@
 @echo off
+:: Define o diretório local chamado "temp"
+setlocal
+set temp_dir=%~dp0temp
+
+:: Cria a pasta "temp" caso não exista
+if not exist "%temp_dir%" mkdir "%temp_dir%"
+
 :: Obtém a data e hora atual no formato yyyy-MM-dd-hh-mm-ss
 for /f "tokens=2 delims==" %%F in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%F
 set year=%datetime:~0,4%
@@ -7,10 +14,10 @@ set day=%datetime:~6,2%
 set hour=%datetime:~8,2%
 set minute=%datetime:~10,2%
 set second=%datetime:~12,2%
-set filename=%year%-%month%-%day%-%hour%-%minute%-%second%.txt
+set filename=%temp_dir%\%year%-%month%-%day%-%hour%-%minute%-%second%.txt
 
-:: Cria o arquivo com o nome gerado
-echo Arquivo criado em %date% %time% > %filename%
+:: Cria o arquivo na pasta local "temp" com o nome gerado
+echo Arquivo criado em %date% %time% > "%filename%"
 
 :: Aguarda 30 segundos
-timeout /t 30 /nobreak >nul
+timeout /t 15 /nobreak >nul
