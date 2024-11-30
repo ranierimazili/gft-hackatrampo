58e4ce5be69fba7a92ae8be0e5666270331570e6
Ranieri Mazili <ranieri85@gmail.com>
2024-11-30 11:50:54 -0300

    Reconhecimento dos domínios github e azure

---
+++ scripts/content.js
@@ -0,0 +1,36 @@
+
+var url = new URL(document.location.href);
+
+if (url.host == "github.com") {
+    console.log("Ta no github, fazer o chablau aqui...")
+} else if (url.host == "dev.azure.com") {
+    console.log("Ta no Azure DevOps, fazer o chablau aqui...")
+} else {
+    console.log("Página ainda não suportada pelo plugin");
+}
+
+/*console.log("Ranieri")
+console.log(document.location.href);
+
+const article = document.querySelector("article");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}*/
