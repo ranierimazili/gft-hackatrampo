240a5a639e721121a6229b401cf7b5503d486309
Ranieri Mazili <ranieri85@gmail.com>
2024-12-04 14:13:29 -0300

    Deleted unsed code, unnecessary comments and code formatting

--- scripts/background.js
+++ scripts/background.js
@@ -6,27 +6,20 @@ chrome.tabs.onUpdated.addListener(function
     (tabId, changeInfo, tab) {
         globalTabId = tabId;
         if (changeInfo.url) {
-            //const pageType = checkPage(changeInfo.url);
-
             chrome.tabs.sendMessage(tabId, {
-                type: "URL_CHANGED",
-                //details: pageType
+                type: "URL_CHANGED"
             })
         }
     }
 );
 
-// Recebe a mensagem do content.js no formato abaixo
-// { eventType: "", "id": ""}
+//Receives the message from content.js and process it
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (['codereview', 'storycreator', 'storytaskcreator'].includes(request.eventType)) {
-        console.log("Ação recebida do botão na página!");
-
         let port = chrome.runtime.connectNative('com.gft.aiimpact');
         port.postMessage({ args: `$${request.eventType}#${request.id}#${request.platform}$` });
 
         port.onMessage.addListener((response) => {
-            console.log("Resposta do Native Messaging Host:", response);
             //Send the event to the tab notifying that the script has finished it's execution
             chrome.tabs.sendMessage(globalTabId, {
                 type: "JOB_FINISHED",
